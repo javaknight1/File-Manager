04ab787768dd4a94b387488ecaa4a8bbe12f94d5
javaknight1 <robertaveryiv@gmail.com>
2013-05-17 20:06:52 -0400

    Removed TODO tag since we don't need them.

--- src/proj1/Main.java
+++ src/proj1/Main.java
@@ -27,7 +27,6 @@ public class Main {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		int totalMem = Integer.parseInt(args[0]);
 		int memBlock = Integer.parseInt(args[1]);
 		String filename = args[2];
